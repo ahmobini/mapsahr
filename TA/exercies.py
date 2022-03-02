5e5f30f3923971d5b55c69478c4f04a736384c11
ahmobini <amir.hussein.mobini@gmail.com>
2022-03-02 15:50:33 +0330

    TA 05-12-1400

--- TA/exercies.py
+++ TA/exercies.py
@@ -1,9 +1,52 @@
-def accum(s):
-    counter = 0
-    output = ""
-    for letter in s:
-        output += letter.upper() + letter.lower() * counter + '-'
-        counter += 1
-    return output[:-1]
+# def odd_or_even(arr):
+#     Sum =0
+#     for i in arr:
+#         Sum += i
+#     if Sum % 2 == 0:
+#         print('Even')
+#     else:
+#         print('Odd')
+#
+# odd_or_even([1,2,0])
+#
 
-print(accum(s='ZpglnRxqenU'))
+
+#
+# def tribonacci(num):
+#     if num == 0:
+#         return 0
+#     elif num == 1 or num == 2 or num == 3:
+#         return 1
+#     else:
+#         return tribonacci(num-1) + tribonacci(num-2) + tribonacci(num-3)
+#
+#
+# def appendibility(num):
+#     temp=[]
+#     for i in range(1, num):
+#         temp.append(tribonacci(i))
+#     return temp
+#
+# print(appendibility(10))
+
+
+def format_duration(seconds):
+    # seconds=0
+    # minutes = 60* seconds
+    # hours = 60 * minutes
+    # days = 24 * hours
+    # years = 365 * days
+    #
+    # if seconds == 0:
+    #     return 'now'
+    # elif 0<seconds<60:
+    #     print(seconds+'seconds')
+    # elif seconds<3600:
+    # #     print(1)
+    # elif seconds%60 == 0:
+    #     print('1 minute')
+    # elif seconds%60 != 0 :
+
+
+
+
