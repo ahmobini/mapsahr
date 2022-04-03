37c7df1fd36c2ce7d8c1f6f87fc7868025efe5c8
ahmobini <amir.hussein.mobini@gmail.com>
2022-04-04 00:28:07 +0430

    slight change

--- bootcamp/Q2-numshortening.py
+++ bootcamp/Q2-numshortening.py
@@ -1,10 +1,10 @@
 def shortening(num):
     num = str(num)
-    result = []
+    final_list = []
     for i in range(len(num)-1):
         temp = int(num.replace(num[i:i+2], str(sum([int(num[i]) , int(num[i+1])]))))
-        result.append(temp)
-    return max(result)
+        final_list.append(temp)
+    return max(final_list)
 
 
 print(shortening(100028))
